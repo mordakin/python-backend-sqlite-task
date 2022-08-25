ea12b4b7d71dede3920deefa5c5358d91a1a7e4d
mordakin <chumik_2002@mail.ru>
2022-08-25 21:43:40 +0300

    Create Table Code Formatted

--- src/db/create_table.py
+++ src/db/create_table.py
@@ -4,18 +4,18 @@ db = sqlite3.connect('C:/Users/chumi/Desktop/DimaTest/Console_app/python-backend
 cur = db.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(255) NOT NULL,
-surname VARCHAR(255) NOT NULL,
-patronymic VARCHAR(255),
-country VARCHAR(255) NOT NULL,
-educational_institution VARCHAR(255) NOT NULL,
-phone_number VARCHAR(50) NOT NULL UNIQUE,
-email VARCHAR(255) NOT NULL UNIQUE,
-password VARCHAR(255) NOT NULL,
-request_status VARCHAR(20)("null"|SENT),
-is_admin BOOLEAN DEFAULT FALSE,
-logged_in BOOLEAN
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    patronymic VARCHAR(255),
+    country VARCHAR(255) NOT NULL,
+    educational_institution VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    request_status VARCHAR(20)("null"|SENT),
+    is_admin BOOLEAN DEFAULT FALSE,
+    logged_in BOOLEAN
 )
 ''')
 
