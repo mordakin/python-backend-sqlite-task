8fb3dd205c85b71e08486b536c4d2a1ed61ff660
mordakin <chumik_2002@mail.ru>
2022-08-27 00:54:26 +0300

    Done insert data through a dictionary

--- src/db/create_table.py
+++ src/db/create_table.py
@@ -4,22 +4,19 @@ import os
 db = sqlite3.connect(os.path.abspath(__file__ + '/../../../corpus.db'))
 cur = db.cursor()
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS users
-(
-    id                      INTEGER PRIMARY KEY AUTOINCREMENT,
-    name                    VARCHAR(255) NOT NULL,
-    surname                 VARCHAR(255) NOT NULL,
-    patronymic              VARCHAR(255),
-    country                 VARCHAR(255) NOT NULL,
-    educational_institution VARCHAR(255) NOT NULL,
-    phone_number            VARCHAR(50)  NOT NULL UNIQUE,
-    email                   VARCHAR(255) NOT NULL UNIQUE,
-    password                VARCHAR(255) NOT NULL,
-    request_status          VARCHAR(20),
-    is_admin                BOOLEAN DEFAULT FALSE,
-    logged_in               BOOLEAN
-)''')
 
-db.commit()
-db.close()
+# cur.execute('''
+# CREATE TABLE IF NOT EXISTS test_dict2
+# (
+#     id                      INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name                    VARCHAR(255) NOT NULL,
+#     surname                 VARCHAR(255) NOT NULL,
+#     patronymic              VARCHAR(255),
+#     email                   VARCHAR(255)
+# )''')
+
+def data_insert(dict_data):
+    cur.execute('INSERT INTO test_dict2 (name,surname,patronymic,email) VALUES (:name,:surname,:patronymic,:email)',
+                dict_data)
+    # cur.execute('UPDATE test_dict2 SET email = :email WHERE name = "aaa"', users)
+    db.commit()
